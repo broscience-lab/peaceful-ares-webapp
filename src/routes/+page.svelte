14b2011ee07e03a4444742d1bdcb7643ec9a0263
Bram Lo <bram_lo@outlook.com>
2023-05-12 23:57:56 +0200

    style: add concept layout for workouts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,197 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<button class="btn-primary btn">Click me 🥹</button>
+<div class="container m-10 mx-auto">
+  <div class="border-b border-t border-dashed border-primary-content">
+    <div class="border-l border-r border-dashed border-primary-content">
+      <div class="px-4 py-8">
+        <div class="pt-10 sm:pt-10 md:pt-24">
+          <div class="text-center">
+            <p
+              class="
+                      font-general-semibold
+                      mb-2
+                      text-2xl
+                      font-semibold
+                      sm:text-5xl
+                     "
+            >
+              Workouts
+            </p>
+          </div>
+
+          <div class="mt-8 sm:mt-10">
+            <h3
+              class="
+                      font-general-regular
+                      text-md
+                      mb-4 text-center
+                      font-normal
+                      sm:text-xl"
+            >
+              Search workouts by title or filter by category
+            </h3>
+
+            <div
+              class="
+                        flex
+                        justify-between
+                        gap-2
+                        border-b
+                        border-primary-content
+                        pb-3"
+            >
+              <div class="flex justify-between gap-2">
+                <input
+                  class="
+                      font-general-medium
+                      border-1
+                      dark:border-secondary-dark
+                      sm:text-md
+                      rounded-lg
+                      border-gray-200 bg-base-300
+                      py-2
+                      pl-3
+                      pr-1
+                      text-sm
+                      sm:px-4
+                    "
+                  id="name"
+                  name="name"
+                  type="search"
+                  required=""
+                  placeholder="Search workout"
+                  aria-label="Name"
+                />
+                <span
+                  class="
+                            hidden
+                            cursor-pointer
+                            rounded-xl
+                            bg-base-300
+                            p-2.5
+                            shadow-sm
+                            sm:block
+
+                  "
+                >
+                  search icon :)
+                </span>
+              </div>
+              <select
+                class="
+                            font-general-medium
+                            border-1
+                            sm:text-md
+                            rounded-lg
+                            border-gray-200
+                            bg-base-300
+                            px-4
+                            py-2
+                            text-sm
+                            "
+              >
+                <option value class="sm:text-md text-sm">All</option>
+                <option class="sm:text-md">Chest and triceps</option>
+                <option class="sm:text-md">Back and Biceps</option>
+                <option class="sm:text-md">Legs and Shoulders</option>
+              </select>
+            </div>
+          </div>
+
+          <div
+            class="grid grid-cols-1 p-3 sm:grid-cols-2 sm:gap-10 lg:grid-cols-3"
+          >
+            <div
+              class="
+                        dark:bg-secondary-dark
+                        mb-10
+                        cursor-pointer
+                        rounded-xl
+                        bg-base-300
+                        shadow-lg
+                        hover:shadow-xl
+                        sm:mb-0
+                        "
+            >
+              <div>
+                <img
+                  src="https://images.unsplash.com/photo-1599577456698-e1e9ae4f4e5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFjayUyMHdvcmtvdXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                  class="rounded-t-xl border-none"
+                />
+              </div>
+              <div class="px-4 py-6 text-center">
+                <p
+                  class="
+                          font-general-semibold
+                          text-ternary-dark dark:text-ternary-light
+                          mb-2
+                          text-xl
+                          font-semibold
+                        "
+                >
+                  Back and Biceps
+                </p>
+              </div>
+
+              <div class="mb-4 flex flex-wrap justify-center">
+                <div>
+                  <span
+                    class="mr-2 rounded bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+                    >Extreme workout</span
+                  >
+                  <span
+                    class="mr-2 rounded bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-300"
+                    >lorum ipsum</span
+                  >
+                </div>
+              </div>
+
+              <div class="mb-4 flex justify-center">
+                <div class="mb-5 mt-2.5 flex items-center">
+                  <svg
+                    class="h-5 w-5 text-red-300"
+                    fill="currentColor"
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 611.999 611.999"
+                    xml:space="preserve"
+                  >
+                    <path
+                      d="M216.02,611.195c5.978,3.178,12.284-3.704,8.624-9.4c-19.866-30.919-38.678-82.947-8.706-149.952
+                      c49.982-111.737,80.396-169.609,80.396-169.609s16.177,67.536,60.029,127.585c42.205,57.793,65.306,130.478,28.064,191.029
+                      c-3.495,5.683,2.668,12.388,8.607,9.349c46.1-23.582,97.806-70.885,103.64-165.017c2.151-28.764-1.075-69.034-17.206-119.851
+                      c-20.741-64.406-46.239-94.459-60.992-107.365c-4.413-3.861-11.276-0.439-10.914,5.413c4.299,69.494-21.845,87.129-36.726,47.386
+                      c-5.943-15.874-9.409-43.33-9.409-76.766c0-55.665-16.15-112.967-51.755-159.531c-9.259-12.109-20.093-23.424-32.523-33.073
+                      c-4.5-3.494-11.023,0.018-10.611,5.7c2.734,37.736,0.257,145.885-94.624,275.089c-86.029,119.851-52.693,211.896-40.864,236.826
+                      C153.666,566.767,185.212,594.814,216.02,611.195z"
+                    />
+                  </svg>
+                  <svg
+                    class="h-5 w-5 text-red-300"
+                    fill="currentColor"
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 611.999 611.999"
+                    xml:space="preserve"
+                  >
+                    <path
+                      d="M216.02,611.195c5.978,3.178,12.284-3.704,8.624-9.4c-19.866-30.919-38.678-82.947-8.706-149.952
+                      c49.982-111.737,80.396-169.609,80.396-169.609s16.177,67.536,60.029,127.585c42.205,57.793,65.306,130.478,28.064,191.029
+                      c-3.495,5.683,2.668,12.388,8.607,9.349c46.1-23.582,97.806-70.885,103.64-165.017c2.151-28.764-1.075-69.034-17.206-119.851
+                      c-20.741-64.406-46.239-94.459-60.992-107.365c-4.413-3.861-11.276-0.439-10.914,5.413c4.299,69.494-21.845,87.129-36.726,47.386
+                      c-5.943-15.874-9.409-43.33-9.409-76.766c0-55.665-16.15-112.967-51.755-159.531c-9.259-12.109-20.093-23.424-32.523-33.073
+                      c-4.5-3.494-11.023,0.018-10.611,5.7c2.734,37.736,0.257,145.885-94.624,275.089c-86.029,119.851-52.693,211.896-40.864,236.826
+                      C153.666,566.767,185.212,594.814,216.02,611.195z"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
